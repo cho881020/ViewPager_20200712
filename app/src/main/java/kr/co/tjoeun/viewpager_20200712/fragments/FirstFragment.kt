5386d911370936b7cc5bd16891e9b9e4699eb864
조경진부캐 <cho881020@gmail.com>
2020-07-12 15:42:08 +0900

    프래그먼트 두개 추가

---
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200712/fragments/FirstFragment.kt
@@ -0,0 +1,30 @@
+package kr.co.tjoeun.fragment_20200712.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
+import kr.co.tjoeun.viewpager_20200712.R
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        changeInfoBtn.setOnClickListener {
+            infoTxt.text = "변경된 문구"
+        }
+
+    }
+
+}
